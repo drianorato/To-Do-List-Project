0ef496f4e63e8410c3de206f065da39a6b98fd65
Drian Orato <drian.orato@gmail.com>
2025-05-30 14:46:48 +0800

    main content and function of to-do list

---
+++ script.js
@@ -0,0 +1,58 @@
+const addbutton = document.getElementById('addTask')
+const taskInput = document.getElementById('taskInput')
+const taskList = document.getElementById('taskList')
+
+loadTasks()
+
+function addTask(){
+
+    const task =  taskInput.value.trim() 
+    if(task){
+        createTaskElement(task)
+        taskInput.value = ""
+
+        saveTasks()
+        
+    }else{
+        alert("Please Enter A Task!")
+    }
+
+}
+
+addbutton.addEventListener("click",addTask)
+
+function createTaskElement(task){
+
+    const listItem = document.createElement('li')
+    
+    listItem.textContent = task
+
+    const deleteButton = document.createElement('button')
+    deleteButton.textContent = 'Delete'
+    deleteButton.className = 'deleteTask' // from css
+
+    listItem.appendChild(deleteButton)
+    taskList.appendChild(listItem)
+
+    deleteButton.addEventListener('click', function(){
+        taskList.removeChild(listItem)
+        saveTasks()
+    })
+}
+
+function saveTasks(){
+
+    let tasks = []
+    taskList.querySelectorAll('li').forEach(function(item){
+        tasks.push(item.textContent.replace('Delete', '').trim())
+    })
+
+    localStorage.setItem('tasks', JSON.stringify(tasks))
+
+}
+function loadTasks(){
+    const tasks = JSON.parse(localStorage.getItem('tasks')) || []
+    tasks.forEach(createTaskElement)
+}
+
+
